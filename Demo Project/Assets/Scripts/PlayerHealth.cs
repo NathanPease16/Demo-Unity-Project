7ebae6fc8c47cb9689fc16d8e45a778e857fcccd
MagiGuy <jdjamrosz@stu.naperville203.org>
2023-10-01 22:37:23 -0500

    Created PlayerHealth file

---
+++ Demo Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health Parameters")]
+    [SerializeField] private float _maxHealth = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
