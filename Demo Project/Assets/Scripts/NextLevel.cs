6e5480abf728e83c729a28825e743db312847429
NathanPease16 <nspease@stu.naperville203.org>
2023-10-23 21:24:45 -0500

    finish Next Level code stuff

--- Demo Project/Assets/Scripts/NextLevel.cs
+++ Demo Project/Assets/Scripts/NextLevel.cs
@@ -6,12 +6,12 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
     // Another Unity message, similar to Update or Awake
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         // Checks to make sure the object that collided is the player, and not some other object with a collider
         if (other.gameObject.CompareTag("Player"))
             // LoadScene will load a scene based on either a given name, or a given number representing the index
             // of that scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
